8e495ba7c9aead4d0cb18072ba00e13a87d25e5b
Austin Smith <austinsmith7531@gmail.com>
2019-05-11 19:18:08 -0400

    mvp finished. The app will take customer orders and deplete the stores inventory

--- main.js
+++ main.js
@@ -1,68 +1,100 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var newItem;
-inquirer
-  .prompt([{
-    type: 'list',
-    name: 'action',
-    message: 'What do you want to do?',
-    choices: [
-      new inquirer.Separator(),
-      'buy',
-      new inquirer.Separator(),
-      'sell',
-      new inquirer.Separator(),
-    ]
-  }, ])
-  .then(answers => {
-    if (answers.action === "buy") {
-      //calls the buy function to initiate the inquirer prompts associated with buying
-      buy()
-    } else {
-      sell()
-    }
-  });
-function buy() {
-  inquirer.prompt([{
-    type: 'list',
-    name: 'action',
-    message: new inquirer.Separator(),
-    choices: [
-      new inquirer.Separator(),
-      "Search by item name",
-      new inquirer.Separator(),
-      "Search by department",
-      new inquirer.Separator(),
-    ]
-  }]).then(answers => {
-    console.log(answers);
-    ///////////////////////what do you want this do do?
-    //needs to call a function that will prompt the user with the names of all the results of their query items from the sql database that fit the search query and call 
-  })
-}
-function sell() {
-  inquirer.prompt([{
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of your item?'
-    },
-    {
+
+//connecting the js to the mysql database
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Zenziethewonderdog26!",
+  database:"bamazon"
+});
+con.connect(function(err,res) {
+  if (err) throw err;
+start();
+});
+function start(){
+  inquirer
+    .prompt([{
       type: 'list',
-      name: "dept",
-      message: 'What department should it me categorized into?',
+      name: 'action',
+      message: 'What do you want to do?',
       choices: [
-        'Home',
-        'Garden',
-        'Computers',
-        'Electronics',
-        "Toys"
+      
+        'buy',
+        new inquirer.Separator(),
+        'sell',
+  
       ]
-    }
-  ]).then(answers => {
-    var item = answers;
-    uploadItem(answers);
-    /////////////////////////what do you want this to do?
-    //calll a function that will push the answers object into the MySQL database
-    //--pass in answers object as a parameter of the function
-  })
-}
+    }, ])
+    .then(answers => {
+      if (answers.action === "buy") {
+        //calls the buy function to initiate the inquirer prompts associated with buying
+        buy();
+      } else if (answers.action === "sell") {
+        sell();
+      }else{
+        con.end;
+      }
+    });
+  }
+
+function buy() {
+  con.query("SELECT * FROM inventory", function(err, results) {
+    if (err) throw err;
+   
+    // once you have the items, prompt the user for which they'd like to bid on
+    var inventoryArray = [];
+    inquirer
+      .prompt([
+        {
+          name: "name",
+          type: "rawlist",
+          choices: function() {
+            for (var i = 0; i < results.length; i++) {
+              inventoryArray.push(results[i]);
+            }
+            return inventoryArray;
+          },
+          message: "What item would you like to buy?"
+        },
+        {
+          name: "quantity",
+          type: "number",
+          message: "How many would you like to purchase?"
+        }
+      ]).then(order=>{
+        
+        for (var i = 0; i < inventoryArray.length; i++) {
+          if (inventoryArray[i].name==order.name){
+            
+            
+           var inStock = parseInt(inventoryArray[i].quantity)
+           console.log(inStock)
+           console.log(order.quantity)
+            if (inventoryArray[i].quantity>order.quantity){
+              inStock -= order.quantity
+              con.query(
+                "UPDATE inventory SET ? WHERE ?",
+                [
+                  {
+                   quantity : inStock
+                  },
+                  {
+                    id:inventoryArray[i].id
+                  }
+                ],
+                function(error) {
+                  if (error) throw err;
+                  console.log("Order placed successfully!");
+                  start();
+                }
+              );
+            }
+          }
+        }
+        console.log(inventoryArray)
+        return inventoryArray;
+      })
+    })
+  }
